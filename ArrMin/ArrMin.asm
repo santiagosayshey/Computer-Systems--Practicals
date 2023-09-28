edd95e6cdc5941d30d28acafce4ba47981d0ae71
Samuel Chau <chaucted@gmail.com>
2023-09-28 15:15:40 +0930

    fixed overflow edge cases

--- ArrMin/ArrMin.asm
+++ ArrMin/ArrMin.asm
@@ -8,42 +8,43 @@ D=M
 @R0
 M=D  // R0 = first element of the array
 
-// Set R3 as the counter (equivalent to a 'for loop' iterator in high-level languages)
+// Set R3 as the counter
 @R2
 D=M
 @R3
 M=D  // R3 = R2 (length of the array)
 
-// Check if the array length is 0, if so, skip the loopa
+// Check if the array length is 0, if so, skip the loop
 @R3
 D=M
 @END
 D;JEQ  // if array length is 0, jump to END
 
-// Loop begins
 (LOOP)
-
 // Load the next element from the array
 @R1
 A=M
 D=M   // D = *R1 (current array element)
 
 @R0
-D=M-D // D = R0 - current array element
+D=D-M // D = current array element - R0
 
+// Check for Overflow, when one number is positive and another is negative
+@CHECK_OVERFLOW
+D;JLT  // If D<0 (signs were different), jump to CHECK_OVERFLOW
+
+// Here, D>=0, no overflow occurred, now we can safely compare
 @CONTINUE
 D;JGE  // If D >= 0 (R0 >= current array element), jump to CONTINUE
 
-// If we are here, the current array element is smaller than R0
+// Update R0 with the new smaller value
 @R1
 A=M
 D=M
 @R0
-M=D  // Update R0 with the new smaller value
+M=D
 
-// Labels for clarity
 (CONTINUE)
-
 // Move to the next array element
 @R1
 M=M+1
@@ -60,3 +61,24 @@ D;JGT  // If R3 > 0, continue the loop
 (END)
 @END
 0;JMP
+
+(CHECK_OVERFLOW)
+@R0
+D=M    // Load R0
+
+@R1
+A=M
+D=D-M // D = R0 - *R1
+
+@CONTINUE
+D;JLT  // If D < 0 (R0 < *R1), jump to CONTINUE
+
+// If we are here, *R1 < R0, so update R0
+@R1
+A=M
+D=M
+@R0
+M=D
+
+@CONTINUE
+0;JMP  // Jump to CONTINUE unconditionally
--- Mult/Mult.asm
+++ Mult/Mult.asm
@@ -12,81 +12,54 @@
 
 // Initialization
 @R0
-M=0 // Clear R0 to store the result.
-
-@R1
-D=M
-@R4
-M=D // Store value of R1 in R4.
-
+M=0
 @R2
 D=M
-@R3
-M=D // Store value of R2 in R3.
 
 @R5
-M=0 // Set R5 as the sign flag, 0 is positive.
+M=0          // This will be our flag. 0 for positive R2, and 1 for negative R2.
 
-// Check if either R1 or R2 is zero.
-@R4
-D=M
-@END
-D;JEQ // If R1 is zero, jump to END.
+@NEGATIVE
+D;JLT
 
 @R3
-D=M
-@END
-D;JEQ // If R2 is zero, jump to END.
+M=D
+@CHECK
+0;JMP
 
-// Check if R1 is negative.
-@R4
-D=M
-@CHECK_R2
-D;JGE // If R1 is positive, jump to CHECK_R2.
-
-@R4
-M=-D // Make R1 positive.
+(NEGATIVE)
+@R3
+M=-D
 @R5
-M=!M // Flip the sign flag.
+M=1          // Mark that R2 was negative.
+@CHECK
+0;JMP
 
-// Check if R2 is negative.
-(CHECK_R2)
-@R3
-D=M
-@MULTIPLY
-D;JGE // If R2 is positive, jump to MULTIPLY.
-
-@R3
-M=-D // Make R2 positive.
-@R5
-M=!M // Flip the sign flag.
-
-// Start of the multiplication loop.
-(MULTIPLY)
+// Begin the multiplication loop
+(CHECK)
 @R3
 D=M
 @END
-D;JEQ // If R2 is zero, jump to END.
+D;JEQ
 
-@R4
+(ADD)
+@R1
 D=M
 @R0
-M=D+M // Add R1 to R0.
-
+M=M+D
 @R3
-MD=M-1 // Decrement R2.
-
-@MULTIPLY
-D;JGT // If R2 is greater than zero, jump to MULTIPLY.
-
-// Adjust the sign of the result if needed.
-@R5
-D=M
-@END
-D;JEQ // If the sign flag is zero, jump to END.
-
-@R0
-M=-M // Negate the result in R0.
+M=M-1
+@CHECK
+0;JMP
 
 (END)
+@R5
+D=M
+@NO_NEGATE
+D;JEQ
 
+// Negate R0
+@R0
+M=-M
+
+(NO_NEGATE)
