54709dc198e3166949043b8252956997fb55c25a
Samuel Chau <chaucted@gmail.com>
2023-09-28 14:41:19 +0930

    a

--- ArrMin/ArrMin.asm
+++ ArrMin/ArrMin.asm
@@ -1,45 +1,62 @@
+// Finds the smallest element in the array of length R2 whose first element is at RAM[R1] and stores the result in R0.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
+// Initialization: Set R0 to the first element in the array
 @R1
 A=M
 D=M
 @R0
-M=D
+M=D  // R0 = first element of the array
 
+// Set R3 as the counter (equivalent to a 'for loop' iterator in high-level languages)
 @R2
 D=M
 @R3
-M=D  // Initialize R3 as the counter
+M=D  // R3 = R2 (length of the array)
 
+// Check if the array length is 0, if so, skip the loop
 @R3
 D=M
 @END
-D;JEQ  // If array length is 0, end the program
+D;JEQ  // if array length is 0, jump to END
 
+// Loop begins
 (LOOP)
+
+// Load the next element from the array
 @R1
 A=M
-D=M   // Load the current element
+D=M   // D = *R1 (current array element)
 
 @R0
-D=M-D // Subtract from the current minimum value
+D=D-M // D = current array element - R0 (smallest element found so far)
 
-@NOT_UPDATE
-D;JGE // If the result is >=0, do not update R0
+@CONTINUE
+D;JGE  // If D >= 0, jump to CONTINUE (this means current array element is greater or equal to the smallest found)
 
+// If we are here, the current array element is smaller than R0
 @R1
 A=M
-D=M   // Update R0 with the new smaller value
+D=M
 @R0
-M=D
+M=D  // Update R0 with the new smaller value
 
-(NOT_UPDATE)
+// Labels for clarity
+(CONTINUE)
+
+// Move to the next array element
 @R1
-M=M+1 // Move to the next element in the array
+M=M+1
 
+// Decrement our loop counter (R3)
 @R3
 M=M-1
-D=M   // Decrement counter
+@R3
+D=M
 @LOOP
-D;JGT // If more elements are there, repeat the loop
+D;JGT  // If R3 > 0, continue the loop
 
+// End of program
 (END)
-0;JMP  // End of the program
+@END
+0;JMP
